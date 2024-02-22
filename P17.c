f445abbe2472d5d94bd16d87dbe5ccca1862f44e
MOHIT-conqueror <160039452+MOHIT-conqueror@users.noreply.github.com>
2024-02-22 21:29:26 +0530

    Create P17.c

---
+++ P17.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+
+void sorting(int a, int b) {
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+void bubbleSort(int arr[], int size) {
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(&arr[j], &arr[j + 1]);
+            }
+        }
+    }
+}
+
+
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int size;
+
+    printf("Enter the size of the array: ");
+    scanf("%d", &size);
+
+    int arr[size];
+
+    printf("Enter the elements of the array:\n");
+    for (int i = 0; i < size; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    
+    bubbleSort(arr, size);
+
+    printf("Sorted array in ascending order: ");
+    printArray(arr, size);
+
+    return 0;
+}
